aed003b1b7d8edacf85730840d1c697a18703655
Niyaz <nmingaripov@gmail.com>
2022-07-02 12:34:07 +0300

    Some PEP8 fix's

--- materials/practice/1.py
+++ materials/practice/1.py
@@ -1,10 +1,13 @@
 # http://cs.mipt.ru/python/lessons/lab1.html
 import numpy as np
 
+
 def y(x):
-    x = float(x) #Зачем флотить x?
-    y = np.log(np.exp(1 / (np.sin(x) + 1)) / (5 / 4 + 1 / (x ** 1 * 5)))/np.log(1 + x ** x)
+    # Зачем флотить x?
+    x = float(x)
+    y = np.log(np.exp(1/(np.sin(x) + 1)) / (5/4 + 1/(x**1 * 5)))/np.log(1 + x**x)
     return y
+
 
 A = [1, 10, 100]
 print(list(map(y, A)))
--- materials/practice/example_balls.py
+++ materials/practice/example_balls.py
@@ -5,6 +5,7 @@ from random import randint
 WIDTH = 300
 HEIGHT = 200
 
+
 class Ball:
     def __init__(self):
         self.R = randint(20, 50)
@@ -15,7 +16,7 @@ class Ball:
                                           self.y - self.R,
                                           self.x + self.R,
                                           self.y + self.R, fill='green')
-    
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
@@ -31,15 +32,16 @@ class Ball:
 def canvas_click_handler(event):
     print('Hello World! x=', event.x, 'y=', event.y)
 
+
 def tick():
     for ball in balls:
         ball.move()
         ball.show()
     root.after(50, tick)
 
+
 def main():
     global root, canvas, balls
-    
     root = tk.Tk()
     root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
     canvas = tk.Canvas(root)
@@ -48,6 +50,7 @@ def main():
     balls = [Ball() for i in range(5)]
     tick()
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
